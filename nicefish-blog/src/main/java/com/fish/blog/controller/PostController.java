d79e5b136e6ff51a7d97ee19ed2a37274cd2b786
damoqiongqiu <damoqiongqiu@126.com>
2019-01-04 13:16:43 +0800

    初步完成发表文章功能

--- nicefish-blog/src/main/java/com/fish/blog/controller/PostController.java
+++ nicefish-blog/src/main/java/com/fish/blog/controller/PostController.java
@@ -9,10 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PostController {
@@ -22,19 +19,25 @@ public class PostController {
 	private PostRepository postRepository;
 
 	//TODO:每页显示的条数改为系统配置项
-	@RequestMapping(value = "/blog/post/{page}", method = RequestMethod.GET)
+	@RequestMapping(value = "/blog/post-list/{page}", method = RequestMethod.GET)
 	public ResponseEntity<Object> getPostList(@PathVariable(value="page",required = false) Integer page) {
 		if(page==null||page<=0){
 			page=1;
 		}
 		page=page-1;
-		Page<PostEntity> postEntities=postRepository.findAll(new PageRequest(page,5));
+		Page<PostEntity> postEntities=postRepository.findAll(new PageRequest(page,10));
 		return new ResponseEntity<>(postEntities, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/blog/post-detail/{id}",method = RequestMethod.GET)
-	public  ResponseEntity<Object> getPostDetail(@PathVariable(value = "id",required = true) Integer id){
+	public ResponseEntity<Object> getPostDetail(@PathVariable(value = "id",required = true) Integer id){
 		PostEntity postEntity=postRepository.findOne(id);
+		return new ResponseEntity<>(postEntity, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/blog/write-post",method = RequestMethod.POST)
+	public ResponseEntity<Object> writePost(@RequestBody PostEntity postEntity){
+		postEntity=postRepository.save(postEntity);
 		return new ResponseEntity<>(postEntity, HttpStatus.OK);
 	}
 }
