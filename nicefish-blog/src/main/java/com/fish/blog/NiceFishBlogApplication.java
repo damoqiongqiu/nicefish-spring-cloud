8a27cb2e442304520e0778ece3fa66aca5020011
damoqiongqiu <damoqiongqiu@126.com>
2019-01-15 11:21:30 +0800

    修改Maven的打包配置，以及Module之间的依赖关系，删掉重复定义的代码

--- nicefish-blog/src/main/java/com/fish/blog/NiceFishBlogApplication.java
+++ nicefish-blog/src/main/java/com/fish/blog/NiceFishBlogApplication.java
@@ -3,14 +3,18 @@ package com.fish.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
+ * blog模块会依赖user-center模块中的FishJwtTokenConverter、SecurityConfig、FishCorsFilter等类，
+ * 所以这里加了@ComponentScan注解，扫描blog和user两个二级模块下的Bean
  * @author 大漠穷秋
  */
 @SpringBootApplication
 @EnableResourceServer
 @EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.fish.blog","com.fish.user"})
 public class NiceFishBlogApplication {
 
     public static void main(String[] args) {
--- nicefish-blog/src/test/java/com/fish/blog/NiceFishBlogApplicationTests.java
+++ nicefish-blog/src/test/java/com/fish/blog/NiceFishBlogApplicationTests.java
@@ -12,6 +12,5 @@ public class NiceFishBlogApplicationTests {
     @Test
     public void contextLoads() {
     }
-
 }
 
